0c81ace38af76595d0c9678351a6fcbc0b88532a
KamilMad <kamilul085@gmail.com>
2024-01-23 16:05:38 +0100

    add generic method for extracting one claim

--- src/main/java/com/kamil/jwtauth/security/JwtService.java
+++ src/main/java/com/kamil/jwtauth/security/JwtService.java
@@ -2,19 +2,28 @@ package com.kamil.jwtauth.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-    private final String secretKey =
+    private final String secretKey = "MWE4ZTYwNTY0MjBlNWVmODMyMmY4ZWQ0YzFkODlmODllYWI5ZDFhNDVjNDg1N2IzZTU0YjEwYjUzZDZjYjM1ZA==";
     public String extractUsername(String token) {
-        return null;
+        return getClaim(token, claims -> claims.getSubject());
     }
 
-    private Claims getAllClaims(String token) {
+    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
         return Jwts.
                 parserBuilder()
                 .setSigningKey(getSignInKey())
@@ -24,6 +33,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        String key = secretKey;
+        byte[] keyBytes  = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
