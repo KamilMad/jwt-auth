6a999ee8319d5fe538dc5f0b45b230f194354662
KamilMad <kamilul085@gmail.com>
2024-01-23 16:12:16 +0100

    add generateToken()

--- src/main/java/com/kamil/jwtauth/security/JwtService.java
+++ src/main/java/com/kamil/jwtauth/security/JwtService.java
@@ -1,12 +1,16 @@
 package com.kamil.jwtauth.security;
 
+import com.kamil.jwtauth.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -14,13 +18,26 @@ public class JwtService {
 
     private final String secretKey = "MWE4ZTYwNTY0MjBlNWVmODMyMmY4ZWQ0YzFkODlmODllYWI5ZDFhNDVjNDg1N2IzZTU0YjEwYjUzZDZjYjM1ZA==";
     public String extractUsername(String token) {
-        return getClaim(token, claims -> claims.getSubject());
+        return extractClaim(token, claims -> claims.getSubject());
     }
 
-    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
-
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            User user) {
+
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
