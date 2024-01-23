06e44edde97fb418f0cbbe8874449f2b4c032c40
KamilMad <kamilul085@gmail.com>
2024-01-23 15:47:24 +0100

    add getAllClaims()

--- src/main/java/com/kamil/jwtauth/security/JwtService.java
+++ src/main/java/com/kamil/jwtauth/security/JwtService.java
@@ -1,7 +1,29 @@
 package com.kamil.jwtauth.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private final String secretKey =
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.
+                parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        String key = secretKey;
+    }
 }
